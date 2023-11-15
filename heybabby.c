dbf523aefb3b494abc20591e0f2ab14ab1d058e6
Josiahalabi <Josiahalabi@gmail.com>
2023-11-15 13:24:21 -0800

    laughing

---
+++ heybabby.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+/* File: Heybabby.c
+ * Author: Josiah Alabi
+ * Description:My best project
+*/
+ String (String a, String b){
+	// This is the code with its functions
+	return a + b }
+	int main(void) {
+	string a;
+	string b;
+       	return 0;
+}
+        if (condition)
+{
+       // Code goes here
+}
+       betty your_file_name.c
+
+
+
